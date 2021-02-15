75853205488e84a8ae62dab8289a581be671d673
AmirAbbas Ranjbar <amir_abbas_r@yahoo.com>
2021-02-15 10:00:53 +0330

    times table

--- ListView/app/src/main/java/com/amirranjbar/listview/MainActivity.java
+++ ListView/app/src/main/java/com/amirranjbar/listview/MainActivity.java
@@ -7,36 +7,62 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lstView;
 
+    public void generateTimesTable ( int timesTableNumber){
+        ArrayList<String> tableContent = new ArrayList<String>();
+        for (int j = 1; j <= 10; j++) {
+            tableContent.add(Integer.toString(j * timesTableNumber));
+        }
+        ArrayAdapter<String> tableAddapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tableContent);
+        lstView.setAdapter(tableAddapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lstView = (ListView)findViewById(R.id.lstView);
+         lstView = (ListView) findViewById(R.id.lstView);
+        SeekBar timesSeekBar = (SeekBar) findViewById(R.id.timesSeekBar);
 
-        ArrayList<String> myFamilly = new ArrayList<String>();
-        myFamilly.add("Ali");
-        myFamilly.add("Amir");
-        myFamilly.add("Reza");
-        myFamilly.add("Mamad");
-        myFamilly.add("Nick");
-        myFamilly.add("Gholi");
+        timesSeekBar.setMax(20);
+        timesSeekBar.setProgress(10);
 
-        ArrayAdapter<String> famillyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamilly);
-        lstView.setAdapter(famillyAdapter);
-        lstView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+        timesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, myFamilly.get(position), Toast.LENGTH_SHORT).show();
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int min = 1;
+                int timesTableNumber;
+                if (progress < min) {
+                    timesTableNumber = min;
+                    timesSeekBar.setProgress(min);
+                } else {
+                    timesTableNumber = progress;
+                }
+                generateTimesTable(timesTableNumber);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
+
 
     }
 }
