0e557162db2adad0ebf0911df939d80a48d973ff
AmirAbbas Ranjbar <amir_abbas_r@yahoo.com>
2021-02-15 19:16:34 +0330

    ui compeleted

--- BrainTrain/app/src/main/java/com/amirranjbar/braintrain/MainActivity.java
+++ BrainTrain/app/src/main/java/com/amirranjbar/braintrain/MainActivity.java
@@ -3,12 +3,27 @@ package com.amirranjbar.braintrain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+
+    public void btnGo_onClick(View view) {
+        btnGo.setVisibility(View.INVISIBLE);
+
+    }
+    public void chooseAnswer(View view) {
+        btnGo.setVisibility(View.INVISIBLE);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGo = (Button) findViewById(R.id.btnGo);
     }
 }
