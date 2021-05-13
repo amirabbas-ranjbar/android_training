c3940d84b63972dcbcffd93173896f868f12cb48
AmirAbbas Ranjbar <amir_abbas_r@yahoo.com>
2021-05-13 21:30:17 +0430

    get data from api

---
+++ NewsReader/app/src/main/java/com/amirranjbar/newsreader/DownloadTask.java
@@ -0,0 +1,43 @@
+package com.amirranjbar.newsreader;
+
+import android.os.AsyncTask;
+import android.provider.Telephony;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+        String result = "";
+        URL url;
+        HttpURLConnection urlConnection = null;
+        try {
+            url = new URL(urls[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            int data = inputStreamReader.read();
+            while (data != -1) {
+                char current = (char) data;
+                result += current;
+                data = inputStreamReader.read();
+            }
+            JSONObject jsonObject = new JSONObject(result);
+            String serviceData = jsonObject.getString("data");
+            return serviceData;
+        } catch (Exception ex) {
+            String exMessage = "doInBackground : " + String.valueOf(ex);
+            Log.i("DebugException ", exMessage);
+        }
+
+        return null;
+    }
+}
--- NewsReader/app/src/main/java/com/amirranjbar/newsreader/MainActivity.java
+++ NewsReader/app/src/main/java/com/amirranjbar/newsreader/MainActivity.java
@@ -2,14 +2,40 @@ package com.amirranjbar.newsreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+    SQLiteDatabase smartDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //smartDB = this.openOrCreateDatabase("PardikCloud", MODE_PRIVATE, null);
+        //smartDB.execSQL("CREATE TABLE IF NOT EXISTS smart_types (id INTEGER PRIMARY KEY ,title VARCHAR  ) ");
+
+
+        DownloadTask task = new DownloadTask();
+        try {
+            String serviceData = String.valueOf(task.execute("https://srv.pardik.co/MirrorService/SmartHomeTypes").get());
+            Log.i("TestDebug", String.valueOf(serviceData));
+        } catch (Exception ex) {
+            Log.i("DebugException", ex.getMessage());
+        }
+
+        ListView listview = findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, titles);
+        listview.setAdapter(arrayAdapter);
     }
 }
