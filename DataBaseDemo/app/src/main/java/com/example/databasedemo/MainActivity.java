315b313d1ab0710029405fc260085bb153892bb1
AmirAbbas Ranjbar <amir_abbas_r@yahoo.com>
2021-05-13 13:03:47 +0430

    db demo

--- DataBaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.java
+++ DataBaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.databasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // try {
+
+            SQLiteDatabase myDb = this.openOrCreateDatabase("TestDB", MODE_PRIVATE, null);
+            myDb.execSQL("CREATE TABLE IF NOT EXISTS test (id INT(3), name VARCHAR, age INT(3))");
+            myDb.execSQL("INSERT INTO test (id,name,age) VALUES (1,'amir',26)");
+            myDb.execSQL("INSERT INTO test (id,name,age) VALUES (2,'omid',26)");
+            Cursor c = myDb.rawQuery("SELECT * FROM test", null);
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+            c.moveToFirst();
+            while (c != null) {
+                Log.i("age", c.getString(ageIndex));
+                Log.i("name", c.getString(nameIndex));
+                c.moveToNext();
+            }
+        /*} catch (Exception ex) {
+            ex.printStackTrace();
+        }*/
     }
 }
